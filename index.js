32d32d84830150c7a1186bc7f948edbf38123d0c
harish <harishnarnaware75@gmail.com>
2022-07-05 13:44:36 +0530

    Initial

--- index.js
+++ index.js
@@ -20,13 +20,14 @@ button.addEventListener("click", function(e) {
     result3.value=name+sirname.slice(0,1)+domain;
     result4.value=name+domain;
     result5.value=sirname+domain;
-   
     
     
-
+    
+    https://verify.gmass.co/verify?email=harishnarnaware74@gmail.com&key=52D5D6DD-CD2B-4E5A-A76A-1667AEA3A6FC
    
-    url =`https://emailverification.whoisxmlapi.com/api/v2?apiKey=at_MsGBAmjNNV2647y0rIC7ULWVELTbD&emailAddress=${result1.value}`;
-    // url ="https://emailverification.whoisxmlapi.com/api/v2?apiKey=at_MsGBAmjNNV2647y0rIC7ULWVELTbD&emailAddress=babbalrai366@gmail.com";
+    // url =`https://emailverification.whoisxmlapi.com/api/v2?apiKey=at_MsGBAmjNNV2647y0rIC7ULWVELTbD&emailAddress=${result1.value}`;
+    url =`https://verify.gmass.co/verify?email=${result1.value}&key=52D5D6DD-CD2B-4E5A-A76A-1667AEA3A6FC`;
+    
 
     fetch(url).then((response)=>{
        
@@ -35,7 +36,7 @@ button.addEventListener("click", function(e) {
         
         console.log(data1)
         
-        if(data1.smtpCheck){
+        if(data1.Valid){
           result1.classList.add('is-valid')
         }
         else{
@@ -44,8 +45,9 @@ button.addEventListener("click", function(e) {
         
     })
        
-    url =`https://emailverification.whoisxmlapi.com/api/v2?apiKey=at_MsGBAmjNNV2647y0rIC7ULWVELTbD&emailAddress=${result2.value}`;
+    // url =`https://emailverification.whoisxmlapi.com/api/v2?apiKey=at_MsGBAmjNNV2647y0rIC7ULWVELTbD&emailAddress=${result2.value}`;
     // url ="https://emailverification.whoisxmlapi.com/api/v2?apiKey=at_MsGBAmjNNV2647y0rIC7ULWVELTbD&emailAddress=babbalrai366@gmail.com";
+    url =`https://verify.gmass.co/verify?email=${result2.value}&key=52D5D6DD-CD2B-4E5A-A76A-1667AEA3A6FC`;
 
     fetch(url).then((response)=>{
        
@@ -53,9 +55,9 @@ button.addEventListener("click", function(e) {
     }).then((data2)=>{
         
      
-        console.log(data2.smtpCheck)
+        console.log(data2.Valid)
         console.log(data2)
-        if(data2.smtpCheck){
+        if(data2.Valid){
           result2.classList.add('is-valid')
         }
         else{
@@ -65,16 +67,17 @@ button.addEventListener("click", function(e) {
  
     })
        
-    url =`https://emailverification.whoisxmlapi.com/api/v2?apiKey=at_MsGBAmjNNV2647y0rIC7ULWVELTbD&emailAddress=${result3.value}`;
+    // url =`https://emailverification.whoisxmlapi.com/api/v2?apiKey=at_MsGBAmjNNV2647y0rIC7ULWVELTbD&emailAddress=${result3.value}`;
     // url ="https://emailverification.whoisxmlapi.com/api/v2?apiKey=at_MsGBAmjNNV2647y0rIC7ULWVELTbD&emailAddress=babbalrai366@gmail.com";
+    url =`https://verify.gmass.co/verify?email=${result3.value}&key=52D5D6DD-CD2B-4E5A-A76A-1667AEA3A6FC`;
 
     fetch(url).then((response)=>{
        
         return response.json();
     }).then((data3)=>{
-      console.log(data3.smtpCheck)
+      console.log(data3.Valid)
       console.log(data3)
-      if(data3.smtpCheck){
+      if(data3.Valid){
         result3.classList.add('is-valid')
       }
       else{
@@ -83,16 +86,17 @@ button.addEventListener("click", function(e) {
     
     })
        
-    url =`https://emailverification.whoisxmlapi.com/api/v2?apiKey=at_MsGBAmjNNV2647y0rIC7ULWVELTbD&emailAddress=${result4.value}`;
+    // url =`https://emailverification.whoisxmlapi.com/api/v2?apiKey=at_MsGBAmjNNV2647y0rIC7ULWVELTbD&emailAddress=${result4.value}`;
     // url ="https://emailverification.whoisxmlapi.com/api/v2?apiKey=at_MsGBAmjNNV2647y0rIC7ULWVELTbD&emailAddress=babbalrai366@gmail.com";
+    url =`https://verify.gmass.co/verify?email=${result4.value}&key=52D5D6DD-CD2B-4E5A-A76A-1667AEA3A6FC`;
 
     fetch(url).then((response)=>{
        
         return response.json();
     }).then((data4)=>{
-      console.log(data4.smtpCheck)
+      console.log(data4.Valid)
       console.log(data4)
-      if(data4.smtpCheck){
+      if(data4.Valid){
         result4.classList.add('is-valid')
       }
       else{
@@ -101,17 +105,18 @@ button.addEventListener("click", function(e) {
 
     })
        
-    url =`https://emailverification.whoisxmlapi.com/api/v2?apiKey=at_MsGBAmjNNV2647y0rIC7ULWVELTbD&emailAddress=${result5.value}`;
+    // url =`https://emailverification.whoisxmlapi.com/api/v2?apiKey=at_MsGBAmjNNV2647y0rIC7ULWVELTbD&emailAddress=${result5.value}`;
     // url ="https://emailverification.whoisxmlapi.com/api/v2?apiKey=at_MsGBAmjNNV2647y0rIC7ULWVELTbD&emailAddress=babbalrai366@gmail.com";
+    url =`https://verify.gmass.co/verify?email=${result5.value}&key=52D5D6DD-CD2B-4E5A-A76A-1667AEA3A6FC`;
 
     fetch(url).then((response)=>{
        
         return response.json();
     }).then((data5)=>{
         
-        console.log(data5.smtpCheck)
+        console.log(data5.Valid)
         console.log(data5)
-        if(data5.smtpCheck){
+        if(data5.Valid){
           result5.classList.add('is-valid')
         }
         else{
